fff2503552f4e1fc127dba34009830c43f4c500f
idhun90 <idhun90@naver.com>
2022-08-01 22:39:16 +0900

    과제 2: 버튼 클릭 시 맥주 name, image_url, description, pairing food 표시되도록 구현. 이미지 구현을 위해 Kingfisher 라이브러리 사용함.

--- seSACLecutreNetworkBasic/seSACLecutreNetworkBasic/BeerViewController.swift
+++ seSACLecutreNetworkBasic/seSACLecutreNetworkBasic/BeerViewController.swift
@@ -1,29 +1,71 @@
-//
-//  BeerViewController.swift
-//  seSACLecutreNetworkBasic
-//
-//  Created by 이도헌 on 2022/08/01.
-//
-
 import UIKit
+
+import Alamofire
+import Kingfisher
+import SwiftyJSON
+import SwiftUI
 
 class BeerViewController: UIViewController {
 
+    @IBOutlet weak var beerImageView: UIImageView!
+    @IBOutlet weak var beerNameLabel: UILabel!
+    @IBOutlet weak var paringFoodLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        requestBeerInfo()
+        labelUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func requestBeerInfo() {
+        
+        let url = "https://api.punkapi.com/v2/beers/random"
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                let beerName = json[0]["name"].stringValue
+                let imageUrl = json[0]["image_url"].stringValue
+                let description = json[0]["description"].stringValue
+                let foodPairing = json[0]["food_pairing"][0].stringValue
+                
+                guard let url = URL(string: imageUrl) else { return }
+                self.beerImageView.kf.setImage(with: url)
+                
+                self.beerNameLabel.text = beerName
+                self.descriptionLabel.text = description
+                self.paringFoodLabel.text = foodPairing
+                
+                print("출력 결과: \(beerName)")
+                print("출력 결과: \(imageUrl)")
+                print("출력 결과: \(description)")
+                print("출력 결과: \(foodPairing)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-    */
 
+    @IBAction func randomButtomClicked(_ sender: UIButton) {
+        requestBeerInfo()
+    }
+}
+
+extension BeerViewController: BeerViewProtocol {
+    
+    func labelUI() {
+        beerNameLabel.font = .systemFont(ofSize: 13, weight: .bold)
+        paringFoodLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        descriptionLabel.font = .systemFont(ofSize: 10)
+
+        descriptionLabel.numberOfLines = 0
+    }
+    
+    
 }
